fe5e66540bfcaf1b837e44bf80ba9097cffd9b2d
Evandru <evandro_remolli.t@outlook.com>
2023-01-30 22:25:54 -0300

    First Commit

---
+++ Game.cs
@@ -0,0 +1,91 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdleGameThread
+{
+    public class Game
+    {
+        double taxa = 0;
+        double lucroDoacao, dinheiro;
+        double precoDoacao = 5;
+        int qntdDoacao = 0;
+        double precoMulti = 10;
+        double clickMulti = 1;
+        int qntdMulti = 0;
+        ConsoleKeyInfo tecla = new();
+        public Game() { }
+
+        public void Click()
+        {
+            while (true)
+            {
+                tecla = Console.ReadKey();
+
+                if (tecla.Key == ConsoleKey.K)
+                {
+                    if (qntdMulti == 0)
+                    {
+                        dinheiro += 1;
+                    }
+                    else
+                    {
+                        dinheiro += clickMulti * 1 / qntdMulti;
+                    }
+                }
+
+                else if (tecla.Key == ConsoleKey.D && dinheiro >= precoDoacao)
+                {
+                    dinheiro -= precoDoacao;
+                    qntdDoacao += 1;
+                    precoDoacao += 5 * taxa;
+                    taxa += 0.1 * qntdDoacao;
+                    lucroDoacao += taxa;
+                }
+
+                else if (tecla.Key == ConsoleKey.M && dinheiro >= precoMulti)
+                {
+                    dinheiro -= precoMulti;
+                    qntdMulti += 1;
+                    precoMulti += 10 * taxa;
+                    taxa += 0.05 * qntdMulti;
+                    clickMulti += taxa / 5;
+                }
+            }
+        }
+
+        public void Farm()
+        {
+            while(true)
+            {
+                dinheiro += lucroDoacao;
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Hud()
+        {
+            while (true)
+            {
+                string hud = $"""
+                                  IDLE GAME 
+
+                     Pressione 'k' para ganhar dinheiro
+                     Pressione 'm' para multiplicar o dinheiro K
+                     Pressione 'd' para aumentar a doação
+
+                     {qntdMulti} multiplicador(es) aumentando {Math.Round(clickMulti, 2)} vezes
+                     {qntdDoacao} doação(ões) recebendo {lucroDoacao.ToString("C")} reais
+
+                     Você possui {dinheiro.ToString("C")} reais
+                    """;
+                Console.WriteLine(hud);
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using IdleGameThread;
+
+Game jogo = new Game();
+
+var click = new Action(jogo.Click);
+var farm = new Action(jogo.Farm);
+var hud = new Action(jogo.Hud);
+
+Parallel.Invoke(click, farm, hud);
